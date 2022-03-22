3335ac3ecd74b61c39cfbdea7e7e3966a169d105
diego-ads-uniaeso <diego.pereira@aluno.uniaeso.edu.br>
2022-03-22 18:20:43 -0300

    Ajustes na classe Frase, criação da classe InterfaceFrase e .gitignore para não subir os arquivos .class.

--- src/br/edu/uniaeso/aluno/Main.java
+++ src/br/edu/uniaeso/aluno/Main.java
@@ -1,7 +1,10 @@
 package br.edu.uniaeso.aluno;
 
+import br.edu.uniaeso.aluno.q1.InterfaceFrase;
+
 public class Main {
     public static void main(String[] args) {
-
+        InterfaceFrase interfaceFrase = new InterfaceFrase();
+        interfaceFrase.getFraseAndWords();
     }
 }

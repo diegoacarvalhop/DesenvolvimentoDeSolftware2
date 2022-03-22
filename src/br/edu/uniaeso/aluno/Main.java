252e3bbdf081df2dbae50bc42827b18c3126098e
diego-ads-uniaeso <diego.pereira@aluno.uniaeso.edu.br>
2022-03-22 17:56:07 -0300

    Primeiro Commit

---
+++ src/br/edu/uniaeso/aluno/Main.java
@@ -0,0 +1,7 @@
+package br.edu.uniaeso.aluno;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/br/edu/uniaeso/aluno/q1/Frase.java
@@ -0,0 +1,17 @@
+package br.edu.uniaeso.aluno.q1;
+
+public class Frase {
+    private String frase;
+
+    public Frase(String frase) {
+        setFrase(frase);
+    }
+
+    public String getFrase() {
+        return frase;
+    }
+
+    public void setFrase(String frase) {
+        this.frase = frase;
+    }
+}

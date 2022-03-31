8b4903c4e15c2a7ed59a558300bde895b8b4fb0c
diego-ads-uniaeso <diego.pereira@aluno.uniaeso.edu.br>
2022-03-31 11:03:37 -0300

    Questão 5: 5.	Faça um programa em java que calcule e mostre a área de um losango. Sabe-se que: A = (diagonal_maior * diagonal_menor)/2

---
+++ src/br/edu/uniaeso/aluno/lista_um/q5/Losango.java
@@ -0,0 +1,35 @@
+package br.edu.uniaeso.aluno.lista_um.q5;
+
+public class Losango {
+    private double diagonal_menor, diagonal_maior;
+
+    public Losango() {
+        setDiagonal_menor(0);
+        setDiagonal_maior(0);
+    }
+
+    public Losango(double diagonal_menor, double diagonal_maior) {
+        setDiagonal_menor(diagonal_menor);
+        setDiagonal_maior(diagonal_maior);
+    }
+
+    public double getDiagonal_menor() {
+        return diagonal_menor;
+    }
+
+    public void setDiagonal_menor(double diagonal_menor) {
+        this.diagonal_menor = diagonal_menor;
+    }
+
+    public double getDiagonal_maior() {
+        return diagonal_maior;
+    }
+
+    public void setDiagonal_maior(double diagonal_maior) {
+        this.diagonal_maior = diagonal_maior;
+    }
+
+    public double calculateArea() {
+        return (getDiagonal_maior() * getDiagonal_menor())/2;
+    }
+}

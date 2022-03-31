e9257820615f4a125192c23e4a9d1a75e4b669e4
diego-ads-uniaeso <diego.pereira@aluno.uniaeso.edu.br>
2022-03-31 11:16:50 -0300

    Questão 6: 6.	Implemente um programa em Java que leia as coordenadas de dois pontos e calcule a distância entre eles. Lembre-se que a distância entre dois pontos é dada pela seguinte equação:  . Utilize as funções sqrt e pow da classe Math para calcular a raiz quadrada e elevar os números as suas devidas potências

---
+++ src/br/edu/uniaeso/aluno/lista_um/q6/InterfaceDistanciaEntreDoisPontos.java
@@ -0,0 +1,81 @@
+package br.edu.uniaeso.aluno.lista_um.q6;
+
+import java.util.Scanner;
+
+public class InterfaceDistanciaEntreDoisPontos {
+    private double user_ponto_x1, user_ponto_x2, user_ponto_y1, user_ponto_y2;
+    private Scanner get;
+
+    public InterfaceDistanciaEntreDoisPontos() {
+        setUser_ponto_x1(0);
+        setUser_ponto_x2(0);
+        setUser_ponto_y1(0);
+        setUser_ponto_y2(0);
+        setGet(new Scanner(System.in));
+    }
+
+    public InterfaceDistanciaEntreDoisPontos(double user_ponto_x1, double user_ponto_x2, double user_ponto_y1, double user_ponto_y2) {
+        setUser_ponto_x1(user_ponto_x1);
+        setUser_ponto_x2(user_ponto_x2);
+        setUser_ponto_y1(user_ponto_y1);
+        setUser_ponto_y2(user_ponto_y2);
+        setGet(new Scanner(System.in));
+    }
+
+    public double getUser_ponto_x1() {
+        return user_ponto_x1;
+    }
+
+    public void setUser_ponto_x1(double user_ponto_x1) {
+        this.user_ponto_x1 = user_ponto_x1;
+    }
+
+    public double getUser_ponto_x2() {
+        return user_ponto_x2;
+    }
+
+    public void setUser_ponto_x2(double user_ponto_x2) {
+        this.user_ponto_x2 = user_ponto_x2;
+    }
+
+    public double getUser_ponto_y1() {
+        return user_ponto_y1;
+    }
+
+    public void setUser_ponto_y1(double user_ponto_y1) {
+        this.user_ponto_y1 = user_ponto_y1;
+    }
+
+    public double getUser_ponto_y2() {
+        return user_ponto_y2;
+    }
+
+    public void setUser_ponto_y2(double user_ponto_y2) {
+        this.user_ponto_y2 = user_ponto_y2;
+    }
+
+    public Scanner getGet() {
+        return get;
+    }
+
+    public void setGet(Scanner get) {
+        this.get = get;
+    }
+
+    public void getUser() {
+        System.out.println("Informe o valor para x1:");
+        setUser_ponto_x1(getGet().nextDouble());
+        System.out.println("Informe o valor para x2:");
+        setUser_ponto_x2(getGet().nextDouble());
+        System.out.println("Informe o valor para y1:");
+        setUser_ponto_y1(getGet().nextDouble());
+        System.out.println("Informe o valor para y2:");
+        setUser_ponto_y2(getGet().nextDouble());
+    }
+
+    public double calculateDistancia() {
+        DistanciaEntreDoisPontos d = new DistanciaEntreDoisPontos(getUser_ponto_x2(), getUser_ponto_x2(), getUser_ponto_y1(), getUser_ponto_y2());
+        return d.calculateDistancia();
+    }
+
+}

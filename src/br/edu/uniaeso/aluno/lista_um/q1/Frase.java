df3948f1139d4c4425ff328bed0439fc42ea10a0
diegopereira <diegoacarvalhop@hotmail.com>
2022-05-23 20:11:37 -0300

    Criação da classe Frase e da classe de interface do usuário

---
+++ src/br/edu/uniaeso/aluno/lista_um/q1/Frase.java
@@ -0,0 +1,40 @@
+package br.edu.uniaeso.aluno.lista_um.q1;
+
+public class Frase {
+    private String frase, wordFrase, wordReplace;
+
+    public Frase(String frase, String wordFrase, String wordReplace) {
+        setFrase(frase);
+        setWordFrase(wordFrase);
+        setWordReplace(wordReplace);
+    }
+
+    public String getFrase() {
+        return frase;
+    }
+
+    public void setFrase(String frase) {
+        this.frase = frase;
+    }
+
+    public String getWordFrase() {
+        return wordFrase;
+    }
+
+    public void setWordFrase(String wordFrase) {
+        this.wordFrase = wordFrase;
+    }
+
+    public String getWordReplace() {
+        return wordReplace;
+    }
+
+    public void setWordReplace(String wordReplace) {
+        this.wordReplace = wordReplace;
+    }
+
+    public String replaceFrase() {
+        setFrase(getFrase().replace(getWordFrase(), getWordReplace()));
+        return getFrase();
+    }
+}
